d9590ca93af602709b5d37719581bdb206e1a46a
Evann_l <evann.brunet54@gmail.com>
2024-03-03 20:42:10 +0100

    js begin

--- main.js
+++ main.js
@@ -1,20 +1,56 @@
-/*
-1. Écrire un code qui calcule la somme des entiers de 0 à 150 et qui l’affiche (résultat=11325). Modifier le
-code pour qu’il fasse la somme des entiers à partir de 4 jusque 100 en allant de 3 en 3 (résultat=1716).
-2. Écrire une fonction parfoisLong() qui simule le lancer d’un dé à 6 faces, l’opération étant répétée
-jusqu’à ce l’on obtienne 6. La fonction écrira le nombre de lancer effectués dans la console avec la
-commande console.log() (au lieu de print()). Pour la génération d’entier aléatoire entre 1 et 6 on
-utilisera : Math.floor(Math.random()*6)+1 . Pour déclencher l’exécution de la fonction on complétera
-<body> avec <body onload="parfoisLong()">
-*/
+//biblio
+
+var crypto = require("crypto");
+
+
+//variable initalization
+
+
+var game_id=0;
+var game_state=-1;
+
+var game_round_question_left="...";
+var game_round_question_right="...";
+var game_round_answer="...";
+
+
+//elements initialization
+var display_element_question_left=0;
+var display_element_question_right=0;
+
 
 function wowo_load()
 {
-	var click_actual=document.getElementById("click_show");
-	click_actual.innerHTML="0";
-	var click_best=document.getElementById("best_show");
-	click_best.innerHTML="0";
+	display_element_question_left=document.getElementById("question_left_0");
+	display_element_question_right=document.getElementById("question_right_0");
+
+	var click_actual=document.getElementById("jsedit");
+	click_actual.innerHTML="loading";
+	wowo_game_restart();
+	wowo_display_refresh();
+	click_actual.innerHTML="loaded";
+
 }
+
+
+function wowo_game_restart()
+{
+	game_round_question_left= "lll";
+	game_round_question_right= "rrr";
+	game_round_answer= "aaa";
+	//game_round_question_left= crypto.randomBytes(20).toString('hex');
+	//game_round_question_right= crypto.randomBytes(20).toString('hex');
+	//game_round_answer= crypto.randomBytes(20).toString('hex');
+}
+
+
+function wowo_display_refresh()
+{
+	display_element_question_left.innerHTML=game_round_question_left;
+	display_element_question_right.innerHTML=game_round_question_right;
+	//display_element_answer.innerHTML="game_round_answer";
+}
+
 
 function cps_refresh()
 {
