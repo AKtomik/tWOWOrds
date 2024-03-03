4a164320ce8f8d1345b9aacfc4e9559af616d0cf
Evann_l <evann.brunet54@gmail.com>
2024-03-03 21:36:14 +0100

    words are random int

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+//--- variables ---
+
+
 //biblio
 
-var crypto = require("crypto");
 
 
 //variable initalization
@@ -17,15 +19,76 @@ var game_round_answer="...";
 //elements initialization
 var display_element_question_left=0;
 var display_element_question_right=0;
+var display_element_answer_1=0;
+var display_element_answer_2=0;
+var display_element_answer_3=0;
 
 
-function wowo_load()
+//--- functions/use ---
+
+
+/**
+ * return random integer from range [0,f_range[
+ * @param {int} f_range define the range for the integer
+ * @returns {int} a random integer in the f_range range
+ */
+function wowo_use_rickroll(f_range)
+{
+  return Math.floor(Math.random() * f_range);
+}
+
+
+//--- functions/game ---
+
+
+
+function wowo_game_restart()
+{
+	game_round_question_left= String(wowo_use_rickroll(1000));
+	game_round_question_right= String(wowo_use_rickroll(1000));
+	game_round_answer= String((parseInt(game_round_question_left)+parseInt(game_round_question_right))/2);
+}
+
+
+//--- functions/display ---
+
+
+/**
+ * loading all elements used by displays fonctions
+ * @function wowo_display_load
+ */
+function wowo_display_load()
 {
 	display_element_question_left=document.getElementById("question_left_0");
 	display_element_question_right=document.getElementById("question_right_0");
+	display_element_answer_1=document.getElementById("answer_1");
+	display_element_answer_2=document.getElementById("answer_2");
+	display_element_answer_3=document.getElementById("answer_3");
+}
 
+function wowo_display_refresh()
+{
+	display_element_question_left.innerHTML=game_round_question_left;
+	display_element_question_right.innerHTML=game_round_question_right;
+	display_element_answer_1.innerHTML=game_round_answer[0];
+	display_element_answer_2.innerHTML=game_round_answer[1];
+	display_element_answer_3.innerHTML=game_round_answer[2];
+	//display_element_answer.innerHTML="game_round_answer";
+}
+
+
+
+//--- functions/action ---
+
+
+function wowo_action_load()
+{
 	var click_actual=document.getElementById("jsedit");
+
 	click_actual.innerHTML="loading";
+
+	wowo_display_load();
+
 	wowo_game_restart();
 	wowo_display_refresh();
 	click_actual.innerHTML="loaded";
@@ -33,23 +96,6 @@ function wowo_load()
 }
 
 
-function wowo_game_restart()
-{
-	game_round_question_left= "lll";
-	game_round_question_right= "rrr";
-	game_round_answer= "aaa";
-	//game_round_question_left= crypto.randomBytes(20).toString('hex');
-	//game_round_question_right= crypto.randomBytes(20).toString('hex');
-	//game_round_answer= crypto.randomBytes(20).toString('hex');
-}
-
-
-function wowo_display_refresh()
-{
-	display_element_question_left.innerHTML=game_round_question_left;
-	display_element_question_right.innerHTML=game_round_question_right;
-	//display_element_answer.innerHTML="game_round_answer";
-}
 
 
 function cps_refresh()
