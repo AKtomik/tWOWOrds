100ed178e45436a7d2f3eda7dc267b1b91c43399
Evann (lw) <evann.brunet54@gmail.com>
2024-03-04 18:35:14 +0100

    check

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
  * you can adjust them.
  */
 const sett_type_letters="abcdefghijklmnopqrstuvwxyz";//case sensive
+const sett_display_upper=false;
 
 
 /**
@@ -25,6 +26,9 @@ let game_round_problem_right="...";
 let game_round_solutions=[];
 let game_round_answer="";
 
+let game_round_answer_good=[];
+let game_round_answer_wrong=[];
+
 /**
  * file variables :
  * in order to store all problems in a map variables.
@@ -33,15 +37,14 @@ let game_round_answer="";
  * where YYY is the end of the second word
  * where AAA (and potentialy BBB) is (are) solutions of the problem
  */
+let file_readed=false;
 let file_problems={"empthy":["soo","lot"]};
 let file_problems_keys=["empthy"];//is needed to acces random key.
-let file_readed=false;
 
 {
 	let file_xml = new XMLHttpRequest();
-	file_xml.onreadystatechange=function() 
-	
 	//ON : file finish reading
+	file_xml.onreadystatechange=function()
 		{//on file state change
 		    if (file_xml.readyState==4 && file_xml.status==200) 
 			{//is finsih readling
@@ -51,11 +54,12 @@ let file_readed=false;
 				for (let i=0;i<here_text_lines.length;i++)
 				{
 					let here_list=here_text_lines[i].split(" ");
-					let k=here_list[0];
+					let k=wowo_use_text_case(here_list[0]);
 					file_problems[k]=[];
 					for (let i=1;i<here_list.length;i++)
 					{
-						file_problems[k].push(here_list[i]);
+						file_problems[k].push(wowo_use_text_case(wowo_use_text_begin(here_list[i])));
+						//must use begin because of the \r
 					}
 					//console.log(k+":"+file_problems[k]);
 				}
@@ -81,6 +85,7 @@ let file_readed=false;
 let display_element_actual=0;
 let display_element_question_left=0;
 let display_element_question_right=0;
+let display_element_answers=0;
 let display_element_answer=[0,0,0];
 
 
@@ -117,6 +122,19 @@ function wowo_use_text_end(f_str)
 	return f_str[3]+f_str[4]+f_str[5];
 }
 
+/**
+ * chane the case to the setting
+ * @param {string} f_str the origin string upper &| lower
+ * @returns the string in the good case
+ */
+function wowo_use_text_case(f_str)
+{
+	if (sett_display_upper)
+		return f_str.toUpperCase();
+	else
+		return f_str.toLowerCase();
+}
+
 
 
 //--- functions/game ---
@@ -128,11 +146,18 @@ function wowo_game_restart()
 	display_element_actual.innerHTML="find the solution!";
 
 	game_round_problem_key=file_problems_keys[wowo_use_rickroll(file_problems_keys.length)];
-	game_round_problem_left=wowo_use_text_begin(game_round_problem_key);
-	game_round_problem_right=wowo_use_text_end(game_round_problem_key);
+	game_round_problem_left=wowo_use_text_case(wowo_use_text_begin(game_round_problem_key));
+	game_round_problem_right=wowo_use_text_case(wowo_use_text_end(game_round_problem_key));
 	game_round_solutions=file_problems[game_round_problem_key];
 	console.log(game_round_problem_key);
 	console.log(game_round_solutions);
+}
+
+function wowo_game_isAnswer(f_str)
+{
+	for (let i=0;i<game_round_solutions.length;i++)
+		if (game_round_solutions[i]===f_str) return true;
+	return false;
 }
 
 
@@ -147,6 +172,7 @@ function wowo_display_load()
 {
 	display_element_question_left=document.getElementById("question_left_0");
 	display_element_question_right=document.getElementById("question_right_0");
+	display_element_answers=document.getElementById("answer");
 	display_element_answer[0]=document.getElementById("answer_1");
 	display_element_answer[1]=document.getElementById("answer_2");
 	display_element_answer[2]=document.getElementById("answer_3");
@@ -156,6 +182,19 @@ function wowo_display_refresh()
 {
 	display_element_question_left.innerHTML=game_round_problem_left;
 	display_element_question_right.innerHTML=game_round_problem_right;
+
+	if (game_round_answer_good.includes(game_round_answer))
+	{
+		display_element_answers.style.color="#00ff00";
+	}
+	else if (game_round_answer_wrong.includes(game_round_answer))
+	{
+		display_element_answers.style.color="#ff0000";
+	}
+	else 
+	{
+		display_element_answers.style.color="#ffff00";
+	}
 
 	//console.log("refresh :"+game_round_answer.length);
 	for (let i=0;i<display_element_answer.length;i++)
@@ -177,12 +216,34 @@ function wowo_action_load()
 	display_element_actual=document.getElementById("jsedit");
 
 	display_element_actual.innerHTML="loading...";
-
+	
+	//initalisation
 	wowo_display_load();
 
-	wowo_game_restart();
-	wowo_display_refresh();
+
+	wowo_action_load_wait();
 }
+
+
+	function wowo_action_load_wait()
+	{
+		if (file_readed)
+		{
+			wowo_action_load_start();
+		}
+		else
+		{
+			console.log("waiting...");
+			setTimeout(wowo_action_load_wait,10);
+		}
+	}
+
+
+	function wowo_action_load_start()
+	{
+		wowo_game_restart();
+		wowo_display_refresh();
+	}
 
 
 	//here_key=here_key.toUpperCase();
@@ -205,16 +266,17 @@ function wowo_action_press(f_event)
 	
 	else if (here_key==="Enter" || here_key===" ")
 	{
-		if (game_round_answer.length===3)
+		//if (game_round_answer.length===3)
 		{
 			here_found=true;
 			console.log("enter!");
+			wowo_action_check();
 		}
 	}
 	
 	else if (here_key.length===1)
 	{
-		here_key=here_key.toLowerCase();
+		here_key=wowo_use_text_case(here_key);
 		for (let char in sett_type_letters)
 		{
 			if (sett_type_letters[char]===here_key)
@@ -242,5 +304,28 @@ function wowo_action_press(f_event)
 	if (here_found) 
 	{
 		wowo_display_refresh();
+	}
+}
+
+function wowo_action_check()
+{
+	if (game_round_answer.length===3)
+	{
+		display_element_actual.innerHTML="checking...";
+
+		let here_edit=true;
+		
+		if (wowo_game_isAnswer(game_round_answer[0]+game_round_answer[1]+game_round_answer[2]))
+		{
+			game_round_answer_good.push(game_round_answer);
+		} else {
+			game_round_answer_wrong.push(game_round_answer);
+		}
+
+		if (here_edit)
+		{
+			wowo_display_refresh();
+		}
+		display_element_actual.innerHTML="check";
 	}
 }
